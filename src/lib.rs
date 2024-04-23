74ed6755f1dd7f100afb6a1feece0045adc06b03
Kevin Boos <kevinaboos@gmail.com>
2024-04-23 13:09:43 -0700

    Update to new version of `robius-open` and `robius-android-env`

--- src/lib.rs
+++ src/lib.rs
@@ -19,22 +19,19 @@ pub fn get_raw_context() -> robius_authentication::RawContext {
 
 pub fn init_android_env() {
     #[cfg(target_os = "android")] {
-        use std::mem::MaybeUninit;
-        use robius_android_env::{JavaVM, JObject};
+        use robius_android_env::{sys, JavaVM};
         use makepad_widgets::makepad_platform::os::linux::android::android_jni::{get_java_vm, get_activity};
 
-        fn activity_getter() -> &'static JObject<'static> {
-            static mut ACTIVITY: MaybeUninit<JObject<'static>> = MaybeUninit::uninit();
-            unsafe {
-                let a = ACTIVITY.write(JObject::from_raw(get_activity().cast()));
-                &*a
-            }
+        fn activity_getter() -> (Option<*mut sys::JNIEnv>, sys::jobject) {
+            (None, get_activity().cast())
         }
 
         let jvm = unsafe { JavaVM::from_raw(get_java_vm().cast()).unwrap() };
         robius_android_env::set_vm(jvm)
             .expect("Failed to set JavaVM");
-        robius_android_env::set_current_activity_getter(activity_getter)
-            .expect("Failed to set current activity getter");
+        unsafe {
+            robius_android_env::set_activity_getter(activity_getter)
+                .expect("Failed to set current activity getter");
+        }
     }
 }
